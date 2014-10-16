9b80722983530b7b20478145f601bd6b57d951c8
Martin Holt Juliussen <martinhj@gmail.com>
2014-10-16 15:31:33 +0200

    Code for testing H-bridge.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,46 +1,30 @@
 #include <Stepper.h>
- 
- const int stepsPerRevolution = 48;  // change this to fit the number of steps per revolution
-                          // for your motor
- 
- // initialize the stepper library on the motor shield
- Stepper myStepper(stepsPerRevolution, 12,13);     
- 
- // give the motor control pins names:
- const int pwmA = 3;
- const int pwmB = 11;
- const int brakeA = 9;
- const int brakeB = 8;
- const int dirA = 12;
- const int dirB = 13;
- 
- int x = 0;
+
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8,9,10,11);            
+
+int stepCount = 0;         // number of steps the motor has taken
+boolean direction = false;
+
 void setup() {
- Serial.begin(9600);
- // set the PWM and brake pins so that the direction pins  // can be used to control the motor:
-pinMode(pwmA, OUTPUT);
- pinMode(pwmB, OUTPUT);
- pinMode(brakeA, OUTPUT);
- pinMode(brakeB, OUTPUT);
- digitalWrite(pwmA, HIGH);
- digitalWrite(pwmB, HIGH);
- digitalWrite(brakeA, LOW);
- digitalWrite(brakeB, LOW);
- 
- // initialize the serial port:
- Serial.begin(9600);
- // set the motor speed (for multiple steps only):
- myStepper.setSpeed(400);
- }
+  // initialize the serial port:
+  Serial.begin(9600);
+}
 
- 
- void loop() {
- 
-  myStepper.step(48);
-  Serial.print("ferdig?");
-  myStepper.step(-48);
-  Serial.println("ferdig2?");
-
-  delay(2000);
-
- }
+void loop() {
+  // step one step:
+  myStepper.setSpeed(100);
+  if (direction) {
+    myStepper.step(stepsPerRevolution * 2);
+  } else {
+    myStepper.step(-stepsPerRevolution * 2);
+  }
+  Serial.print("steps:" );
+  Serial.println(stepCount);
+  stepCount++;
+  direction = direction ? false : true;
+  delay(500);
+}
