31d4d1771baf649d96a785c81b47579152df09ef
Martin Holt Juliussen <martinhj@gmail.com>
2014-10-21 11:06:44 +0200

    added some debugging printout to serial. accelrated start.

--- src/sketch.ino
+++ src/sketch.ino
@@ -7,20 +7,31 @@ const int stepsPerRevolution = 48;  // change this to fit the number of steps pe
 Stepper myStepper(stepsPerRevolution, 8,9,10,11);            
 
 int stepCount = 0;         // number of steps the motor has taken
-boolean direction = false;
+boolean direction = true;
 
 void setup() {
   // initialize the serial port:
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
+  Serial.println("starting");
+  myStepper.setSpeed(20);
+  myStepper.step(stepsPerRevolution);
+  myStepper.setSpeed(40);
+  myStepper.step(stepsPerRevolution);
   myStepper.setSpeed(200);
+  myStepper.step(stepsPerRevolution);
 }
 
 void loop() {
   // step one step:
   if (direction = direction ? false : true) {
-    myStepper.step(stepsPerRevolution * 2);
+    myStepper.step(stepsPerRevolution);
+    Serial.println("step+");
+    digitalWrite(13, HIGH);
   } else {
-    myStepper.step(-stepsPerRevolution * 2);
+    myStepper.step(-stepsPerRevolution);
+    digitalWrite(13, LOW);
+    Serial.println("step-");
   }
-  delay(500);
+  delay(100);
 }
