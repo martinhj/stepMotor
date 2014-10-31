e91f7bb33fe85fd336e44fed0932b2e3af718325
Martin Holt Juliussen <martinhj@gmail.com>
2014-10-31 13:48:51 +0100

    added potensiometer reading and booleans to make it possible to set it running in one direction:

--- src/sketch.ino
+++ src/sketch.ino
@@ -14,27 +14,58 @@
 /* name one of them something with right and one left.
  * One should be inverse.
  */
+
+
+
 AccelStepper stepper1(AccelStepper::FULL4WIRE, 3, 4, 5, 6);
 AccelStepper stepper2(AccelStepper::FULL4WIRE, 8, 9, 10, 11);
 
+int x1, x2;
+boolean x1onedir, x2onedir;
+
 void setup()
 {  
-    stepper1.setMaxSpeed(200.0);
-    stepper1.setAcceleration(200.0);
-    stepper1.moveTo(24);
-    
-    stepper2.setMaxSpeed(200.0);
-    stepper2.setAcceleration(200.0);
-    stepper2.moveTo(-80);
+	Serial.begin(9600);
+	stepper1.setMaxSpeed(200.0);
+	stepper1.setAcceleration(200.0);
+	stepper1.moveTo(24);
+
+	stepper2.setMaxSpeed(200.0);
+	stepper2.setAcceleration(200.0);
+	stepper2.moveTo(24);
 }
 
 void loop()
 {
+		x1 = analogRead(0);
+		x2 = analogRead(1);
+
+		Serial.print(x1);
+		Serial.print(" : ");
+		Serial.println(x2);
     // Change direction at the limits
     if (stepper1.distanceToGo() == 0)
       stepper1.moveTo(-stepper1.currentPosition());
     if (stepper2.distanceToGo() == 0)
       stepper2.moveTo(-stepper2.currentPosition());
-    stepper1.run();
-    stepper2.run();
+    if (x1 < 1000){
+      x1onedir = false;
+			stepper1.run();
+		} else {
+      x1onedir = true;
+			stepper1.runSpeed();
+		}
+		if (x2 < 1000) {
+      x2onedir = false;
+			stepper2.run();
+		} else {
+      x2onedir = true;
+			stepper2.runSpeed();
+		}
 }
+
+
+/*
+void setSpeedAndDirection(int speed, int direction) {
+}
+*/
