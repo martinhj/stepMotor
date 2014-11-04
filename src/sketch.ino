945c2a22c600196dcb6194d877ab975dcc8ec5f5
Martin Holt Juliussen <martinhj@gmail.com>
2014-11-04 02:23:29 +0100

    Added and fixed a lot of rfid code. rest is in the header of the file.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,3 +1,7 @@
+//what's left: run back to read rfid tag
+// reset the information about tag so it doesn't keep running after the tag is removed.
+// reset possiton to null when it has read a tag.
+
 // MultiStepper.pde
 // -*- mode: C++ -*-
 //
@@ -25,16 +29,38 @@
  * Added 'false' as a last argument. will not enable the motors until it's
  * started with enableOutputs() later.
  */
-AccelStepper stepper1(AccelStepper::FULL4WIRE, 3, 4, 5, 6);
-AccelStepper stepper2(AccelStepper::FULL4WIRE, 8, 9, 10, 11);
+AccelStepper stepper1(AccelStepper::HALF4WIRE, 3, 4, 5, 6);
+AccelStepper stepper2(AccelStepper::HALF4WIRE, 8, 9, 10, 11);
+
+int arm[] = {-1, -1};
+
+const int STEPSPERREV = 400;
 
 int x1, x2;
 
+boolean firstRun = true;
+
+
+// potentiometer variables.
+int potLastRead[] = {0,0};
+int potRead = 0;
+int potPort[] = {2,3};
+unsigned long lastPotRead = 0;
+
+
+// rfid variables.
 unsigned long lastRead;
 int RFIDResetPin = 2;
 int RFIDResetPin2 = 12;
 int x1rfidtarget, x2rfidtarget;
-char tag10[13] = "04161F27FDD7";
+// stump
+char tag01[13] = "04161F27FDD7";
+// stump
+char tag02[13] = "04161F27F3D9";
+// wheel
+char tag03[13] = "04161F285471";
+// wheel
+char tag04[13] = "04161F271933";
 int ledPin = 13;
 char tagString[13];
 int index = 0;
@@ -46,18 +72,136 @@ void setup()
   stepper1.setPinsInverted(true, true, true);
   stepper2.setPinsInverted(true, true, true);
 
-	stepper1.setMaxSpeed(200.0);
-	stepper1.setAcceleration(200.0);
+	stepper1.setMaxSpeed(350.0);
+	stepper1.setAcceleration(500.0);
 
-	stepper2.setMaxSpeed(200.0);
-	stepper2.setAcceleration(200.0);
+	stepper2.setMaxSpeed(350.0);
+	stepper2.setAcceleration(500.0);
 
   pinMode(RFIDResetPin, OUTPUT);
-  //digitalWrite(RFIDResetPin, LOW);
+  digitalWrite(RFIDResetPin, LOW);
   pinMode(RFIDResetPin2, OUTPUT);
-  //digitalWrite(RFIDResetPin2, LOW);
+  digitalWrite(RFIDResetPin2, LOW);
   pinMode(ledPin, OUTPUT);
 }
+
+
+
+
+void loop()
+{
+  if (millis() - lastRead > 10000 && millis() - lastPotRead > 2000) readCard();
+  //readCard();
+  
+
+		x1 = constrain(map(analogRead(2), 520, 1010, 0, STEPSPERREV), 0, STEPSPERREV);
+		x2 = constrain(map(analogRead(1), 530, 1010, 0, STEPSPERREV), 0, STEPSPERREV);
+    /*
+    x1 = analogRead(2);
+		x2 = analogRead(1);
+    */
+
+    /*
+    Serial.print(x1);
+    Serial.print(" : ");
+    Serial.print(x2);
+    Serial.print(" : ");
+    Serial.print(stepper1.targetPosition());
+    Serial.print(" : ");
+    Serial.print(stepper1.currentPosition());
+    Serial.print(" :: ");
+    Serial.print(stepper2.targetPosition());
+    Serial.print(" : ");
+    Serial.print(stepper2.currentPosition());
+
+    Serial.println();
+    */
+
+    /*
+    // Change direction at the limits
+    if (stepper1.distanceToGo() == 0)
+      stepper1.moveTo(-stepper1.currentPosition());
+    if (stepper2.distanceToGo() == 0)
+      stepper2.moveTo(-stepper2.currentPosition());
+    */
+
+
+
+
+    if (lastPotRead > 100 && firstRun == true) {
+      firstRun = false;
+      stepper1.setCurrentPosition(0);
+      stepper2.setCurrentPosition(1);
+    }
+    if (millis() - lastPotRead > 1200 && !firstRun) {
+
+      if (arm[1] == 2 || arm[1] == 3) {
+        run(stepper1, true);
+      } else if (arm[1] == 0 || arm[1] == 1) {
+        run(stepper1, true);
+      } else {
+        if (x1 > 210 && x1 <= 380) {
+          move(stepper1, potLastRead[0]);
+        }
+        else if (x1 > 20 && x1 <= 190) {
+          move(stepper1, potLastRead[0]);
+        }
+        else if (x1 > 380){
+          run(stepper1, true);
+        } else if (x1 <= 20 ) {
+          run(stepper1, false);
+        } else if (x1 > 190 && x1 <= 210) {
+          stop(stepper1);
+        }
+      }
+
+
+      if (arm[0] == 2 || arm[0] == 3) {
+        run(stepper2, true);
+      } else if (arm[0] == 0 || arm[0] == 1) {
+        run(stepper2, true);
+      } else {
+      
+        if (x2 > 210 && x2 <= 380) {
+          move(stepper2, potLastRead[1]);
+        }
+        else if (x2 > 20 && x2 <= 190) {
+          move(stepper2, potLastRead[1]);
+        }
+        else if (x2 > 380) {
+          run(stepper2, true);
+        } else if (x2 <= 20 ) {
+          run(stepper2, false);
+        } else if (x2 > 190 && x2 <= 210) {
+          stop(stepper2);
+        }
+      }
+    }
+
+
+  //Serial.println(abs(potLastRead[0] - x1) );
+  if (abs(potLastRead[0] - x1) > 6) {
+    if (stepper1.currentPosition() > 0) {
+      stepper1.moveTo(x1);
+    } else {
+      stepper1.moveTo((x1) - STEPSPERREV);
+    }
+    potLastRead[0] = x1;
+    lastPotRead = millis();
+  }
+  if (abs(potLastRead[1] - x2) > 6) {
+    if (stepper2.currentPosition() > 0) {
+      stepper2.moveTo(x2);
+    } else {
+      stepper2.moveTo((x2) - STEPSPERREV);
+    }
+    potLastRead[1] = x2;
+    lastPotRead = millis();
+  }
+    if (arm[1] != -1) stepper1.run();
+    if (arm[0] != -1) stepper2.run();
+}
+
 
 
 
@@ -89,8 +233,8 @@ void readCard() {
   index = 0;
   x1rfidtarget = stepper1.targetPosition();
   x2rfidtarget = stepper1.targetPosition();
-  stepper1.runToNewPosition(0);
-  stepper2.runToNewPosition(0);
+  //stepper1.runToNewPosition(0);
+  //stepper2.runToNewPosition(0);
   turnOffMotors(); // needs a turnonmotors as well when using disableOutputs.
 
   digitalWrite(RFIDResetPin, LOW);
@@ -109,7 +253,8 @@ void readCard() {
       index ++;
     }
   }
-  checkTag(tagString);
+  // 0 == left
+  checkTag(tagString, 0);
   clearTag(tagString);
 
   index = 0;
@@ -130,88 +275,35 @@ void readCard() {
     }
   }
 
-  checkTag(tagString); //Check if it is a match
+  // 1 == right
+  checkTag(tagString, 1); //Check if it is a match
   clearTag(tagString); //Clear the char of all value
   digitalWrite(RFIDResetPin, LOW);
 
-  stepper1.runToNewPosition(x1rfidtarget);
-  stepper2.runToNewPosition(x2rfidtarget);
+  //stepper1.runToNewPosition(x1rfidtarget);
+  //stepper2.runToNewPosition(x2rfidtarget);
 
 
   turnOnMotors();
 
 
   lastRead = millis();
+
 }
 
-void loop()
-{
-  if (millis() - lastRead > 10000) readCard();
-  
-
-		x1 = analogRead(2);
-		x2 = analogRead(1);
-
-    /*
-    Serial.print(x1);
-    Serial.print(" : ");
-    Serial.print(x2);
-    Serial.print(" : ");
-    Serial.print(stepper1.targetPosition());
-    Serial.print(" : ");
-    Serial.print(stepper1.currentPosition());
-    Serial.print(" :: ");
-    Serial.print(stepper2.targetPosition());
-    Serial.print(" : ");
-    Serial.print(stepper2.currentPosition());
-
-    Serial.println();
-    */
-
-    /*
-    // Change direction at the limits
-    if (stepper1.distanceToGo() == 0)
-      stepper1.moveTo(-stepper1.currentPosition());
-    if (stepper2.distanceToGo() == 0)
-      stepper2.moveTo(-stepper2.currentPosition());
-    */
-
-
-
-
-    if (x1 > 800 && x1 <= 1000) {
-      upperWave(stepper1);
-    }
-    else if (x1 > 550 && x1 <= 750) {
-      downerWave(stepper1);
-    }
-    else if (x1 > 1000){
-			run(stepper1, true);
-		} else if (x1 < 550 ) {
-      run(stepper1, false);
-    } else if (x1 > 750 && x1 <= 800) {
-      stop(stepper1);
-    }
-    if (x2 > 800 && x2 <= 1000) {
-      upperWave(stepper2);
-    }
-    else if (x2 > 550 && x2 <= 750) {
-      downerWave(stepper2);
-    }
-		else if (x2 > 1000) {
-			run(stepper2, true);
-		} else if (x2 < 550 ){
-      run(stepper2, false);
-    } else if (x2 > 750 && x2 <= 800) {
-      stop(stepper2);
-    }
-
-
-    stepper1.run();
-    stepper2.run();
+void move(AccelStepper &stepper, int pot) {
+  pot = pot;
+  if (stepper.targetPosition() < pot - 50 || stepper.targetPosition() > pot + 50 
+    || stepper.currentPosition() == stepper.targetPosition()) {
+    stepper.moveTo(pot + 50);
+  }
+  if (stepper.distanceToGo() == 0 && stepper.targetPosition() == pot + 50) {
+    stepper.moveTo(pot - 50);
+  }
+  if (stepper.distanceToGo() == 0 && stepper.targetPosition() == pot - 50) {
+    stepper.moveTo(pot + 50);
+  }
 }
-
-
 
 void clearTag(char one[]){
 ///////////////////////////////////
@@ -223,25 +315,37 @@ void clearTag(char one[]){
   }
 }
 
-void checkTag(char tag[]){
+void checkTag(char tag[], int armn){
 ///////////////////////////////////
 //Check the read tag against known tags
 ///////////////////////////////////
 
   if(strlen(tag) == 0) return; //empty, no need to contunue
 
-  if(compareTag(tag, tag10)){
-    lightLED(13);
+  if(compareTag(tag, tag01)){
+    Serial.print("stump");
+    arm[armn] = 0; // stump
+  }
+  if(compareTag(tag, tag02)){
+    Serial.print("stump");
+    arm[armn] = 1; // stump
+  }
+  if(compareTag(tag, tag03)){
+    Serial.print("wheel");
+    arm[armn] = 2; // wheel
+  }
+  if(compareTag(tag, tag04)){
+    Serial.print("wheel");
+    arm[armn] = 3; // wheel
+  }
+  else {
+    arm[armn] = -1; // no tag
   }
   Serial.println(tag); //read out any unknown tag
 
 }
 
 boolean compareTag(char one[], char two[]){
-///////////////////////////////////
-//compare two value to see if same,
-//strcmp not working 100% so we do this
-///////////////////////////////////
 
   if(strlen(one) == 0) return false; //empty
 
@@ -258,8 +362,6 @@ void lightLED(int pin){
 ///////////////////////////////////
 //Turn on LED on pin "pin" for 250ms
 ///////////////////////////////////
-  Serial.println(pin);
-
   digitalWrite(pin, HIGH);
   delay(250);
   digitalWrite(pin, LOW);
@@ -269,18 +371,11 @@ void lightLED(int pin){
 
 void upperWave(AccelStepper &stepper) {
   /*
-   * These functions needs to be generalized. Take in a start and a stop
-   * position and state if it's going to keep rotating.
-   */
-  /*
    * || stepper.currentPosition() == stepper.targetPosition()) {
    * This function needs this line when it's checking if it's moving in the
    * correct direction and in correct space.
    */
 
-  /* 
-   * these functions needs to take potmeter vals in where it should move.
-   */
   if (stepper.targetPosition() < 25 || stepper.targetPosition() > 100) {
     stepper.moveTo(100);
   }
@@ -308,8 +403,8 @@ void stop(AccelStepper &stepper) {
 }
 
 void run(AccelStepper &stepper, boolean forward) {
-  if (forward) stepper.moveTo(200);
-  if (!forward) stepper.moveTo(-200);
+  if (forward) stepper.moveTo(STEPSPERREV);
+  if (!forward) stepper.moveTo(-STEPSPERREV);
   if (stepper.distanceToGo() == 0) {
     stepper.setCurrentPosition(0);
   }
@@ -320,10 +415,6 @@ void turnOnMotors() {
   stepper2.enableOutputs();
 }
 void turnOffMotors() {
-  /*
-   * void AccelStepper::disableOutputs  (   ) 
-   * use this one instead (and enableOutputs).
-   */
   ///*
   stepper1.disableOutputs();
   stepper2.disableOutputs();
